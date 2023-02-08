b705425370f7dd253cf784cda8502bd938cbbc21
Job85 <julianjernigan@ymail.com>
2023-02-08 16:10:29 -0500

    deleted: div tag around nav tag, ui tag around li tags, li tags around Link tags

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,23 +2,13 @@ import { Link } from 'react-router-dom';
 
 const Nav = ({ user }) => {
     return (
-        <div>
-            <nav>
-                <ul className='nav-bar'>
-                    <li className='nav-anchor' href='default.asp'>
-                        <Link to='/' >Home</Link>
-                        {/* <Link to='/wallet'>My Wallet</Link> */}
-                    </li>
-                    <li className='nav-anchor' href='locations.asp'>
-                        <Link to='/locations'>Locations</Link>
-                    </li>
-                    <li className='nav-anchor-end' href='create.locations.asp'>
-                        <Link to={`/locations/create/${user?.id}`}>Create A Cache</Link>
-                    </li>
-                    {/* <Link to='/rules'>Rules</Link> */}
-                </ul>
-            </nav>
-        </div>
+        <nav className='navbar'>
+            <Link to='/' >Home</Link>
+            {/* <Link to='/wallet'>My Wallet</Link> */}
+            <Link to='/locations'>Locations</Link>
+            <Link to={`/locations/create/${user?.id}`}>Create A Cache</Link>
+            {/* <Link to='/rules'>Rules</Link> */}
+        </nav>
     )
 }
 
