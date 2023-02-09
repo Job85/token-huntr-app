a3880d2cc44c6c12994a2238822269e7488b3524
Job85 <julianjernigan@ymail.com>
2023-02-09 12:17:57 -0500

    changed links from text to icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationCrosshairs, faCompass } from '@fortawesome/free-solid-svg-icons'
+import { faLocationCrosshairs, faCompass, faHouseChimneyUser, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 
 const Nav = ({ user }) => {
     return (
         <nav className='navbar'>
-            <Link to='/' >home</Link>
+            <Link to='/' ><FontAwesomeIcon icon={faHouseChimneyUser} /></Link>
             {/* <Link to='/wallet'>My Wallet</Link> */}
             <Link to='/locations'><FontAwesomeIcon icon={faLocationCrosshairs} /> <FontAwesomeIcon icon={faCompass} /></Link>
-            <Link to={`/locations/create/${user?.id}`}>Create A Cache</Link>
+            <Link to={`/locations/create/${user?.id}`}><FontAwesomeIcon icon={faCirclePlus} /></Link>
             {/* <Link to='/rules'>Rules</Link> */}
         </nav>
     )
