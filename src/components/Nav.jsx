f412dc5cb78f751b5415734f8d7c2d4d52bb6a57
Job85 <julianjernigan@ymail.com>
2023-02-09 12:07:21 -0500

    installed font awesome svg

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationCrosshairs, faCompass } from '@fortawesome/free-solid-svg-icons'
 
 const Nav = ({ user }) => {
     return (
         <nav className='navbar'>
-            <Link to='/' >Home</Link>
+            <Link to='/' >home</Link>
             {/* <Link to='/wallet'>My Wallet</Link> */}
-            <Link to='/locations'>Locations</Link>
+            <Link to='/locations'><FontAwesomeIcon icon={faLocationCrosshairs} /> <FontAwesomeIcon icon={faCompass} /></Link>
             <Link to={`/locations/create/${user?.id}`}>Create A Cache</Link>
             {/* <Link to='/rules'>Rules</Link> */}
         </nav>
