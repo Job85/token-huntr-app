4eba978ab738cf1f12a6c9c30a0f3683f8e87685
Job85 <julianjernigan@ymail.com>
2023-02-09 15:49:18 -0500

    added margin to: 'Log In' & 'Sign Up' buttons'

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -9,11 +9,11 @@ const Landing = () => {
             <section>
                 <h1>Welcome to Token Huntr</h1>
                 <h2>Log In or Sign Up for New Adventures</h2>
-                <button onClick={() => navigate('/login')}>
-                    Log In Here
+                <button onClick={() => navigate('/login')} className='landing-button'>
+                    Log In
                 </button>
-                <button onClick={() => navigate('/register')}>
-                    Sign Up Here
+                <button onClick={() => navigate('/register')} className='landing-button'>
+                    Sign Up
                 </button>
             </section>
         </div>
