0d59763118c99be7c3875268efec6917a27c4916
Job85 <julianjernigan@ymail.com>
2023-02-11 00:48:45 -0500

    created background image for LocationList page

--- src/pages/LocationList.jsx
+++ src/pages/LocationList.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../components/Card';
+import './PageStyles.css';
+import map from "../assets/explorer-map.jpg";
 
 const LocationList = () => {
 
     return (
-        <div>
+        <div className='background'>
+            {/* <img src={map} alt="explorers map" className='background' /> */}
             <h1>GeoCache Locations</h1>
             <Card />
         </div>
