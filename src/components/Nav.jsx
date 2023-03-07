65cec7b5ca4e6c41ca39d7b2393971ce8bf88e8d
Job85 <julianjernigan@ymail.com>
2023-03-06 21:33:04 -0500

    removed FontAwesome icon faCrossHairs

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,13 @@
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationCrosshairs, faCompass, faHouseChimneyUser, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import { faCompass, faHouseChimneyUser, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 
 const Nav = ({ user }) => {
     return (
         <nav className='navbar'>
             <NavLink to='/' className='nav-anchor nav-anchor-start'><FontAwesomeIcon icon={faHouseChimneyUser} className='nav-icon' /></NavLink>
             {/* <NavLink to='/wallet'>My Wallet</NavLink> */}
-            <NavLink to='/locations' className='nav-anchor'><FontAwesomeIcon icon={faLocationCrosshairs} className='nav-icon' /> <FontAwesomeIcon icon={faCompass} className='nav-icon' /></NavLink>
+            <NavLink to='/locations' className='nav-anchor'><FontAwesomeIcon icon={faCompass} className='nav-icon' /></NavLink>
             <NavLink to={`/locations/create/${user?.id}`} className='nav-anchor nav-anchor-end'><FontAwesomeIcon icon={faCirclePlus} className='nav-icon' /></NavLink>
             {/* <NavLink to='/rules'>Rules</NavLink> */}
         </nav>
