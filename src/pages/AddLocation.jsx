032b5dbfd966ded50fd785fb3e969c93177a4a12
Job85 <joulescodinglab@gmail.com>
2023-03-12 01:25:37 -0500

    deleted Icon and CardFooter from CardStyle import, deleted commented out code

--- src/pages/AddLocation.jsx
+++ src/pages/AddLocation.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios'
-import { CardContainer, CardOverlay, CardHeader, Form, InputWrapper, CardInput, CardFooter, CardButton } from "../components/CardStyle";
+import { CardContainer, CardOverlay, CardHeader, Form, InputWrapper, CardInput, CardButton } from "../components/CardStyle";
 import { useNavigate } from 'react-router-dom'
 
 export const BASE_URL = process.env.NODE_ENV === 'local' ? 'http://localhost3001/api' : 'https://token-huntr-server.onrender.com/api'
